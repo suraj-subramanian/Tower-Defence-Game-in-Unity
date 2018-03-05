087e5ff495a75baa1ea116acb8512602bcce1a69
Suraj Subramanian <suraj.tf6@gmail.com>
2018-03-05 16:07:03 +0530

    Basic Mechanics

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+	public static BuildManager instance;
+
+	void Awake(){
+		if (instance!=null) {
+			Debug.Log ("More than one BuildManger in scene!");
+		}
+		instance = this;
+	}
+
+	public GameObject standartTurretPrefab;
+
+	private GameObject turretToBuild;
+
+	void Start(){
+		turretToBuild = standartTurretPrefab;
+	}
+	public GameObject GetTurretToBuild(){
+		return turretToBuild;
+	}
+
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+	public Color hoverColor;
+
+	public Vector3 positionOffset;
+	private GameObject turret;
+
+
+	private Renderer rend;
+	private Color startColor;
+
+	void Start(){
+		rend = GetComponent <Renderer> ();
+		startColor = rend.material.color;
+	}
+
+	void OnMouseEnter(){
+		rend.material.color = hoverColor;
+		if (turret != null) 
+			rend.material.color = Color.red ;
+	}
+
+	void OnMouseExit(){
+		rend.material.color = startColor;
+	}
+
+	void OnMouseDown(){
+		if (turret != null) {
+			Debug.Log ("Cant build there! -TODO : Display on screen");
+			return;
+		}
+		//Build a turret.
+		GameObject turretToBuild=BuildManager.instance.GetTurretToBuild ();
+		turret=(GameObject)Instantiate (turretToBuild,transform.position+positionOffset,transform.rotation);
+	}
+}
